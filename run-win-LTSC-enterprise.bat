89104352e1195b3ed74bf133a4c3b12f31eb14f8
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2024-09-03 12:15:21 +0100

    Update run-win-LTSC-enterprise.bat

--- run-win-LTSC-enterprise.bat
+++ run-win-LTSC-enterprise.bat
@@ -4,7 +4,7 @@ cls
 echo Downloading dependencies...
 echo please wait
 echo.
-echo (if you are prompted to replace a file, type a capital N, and press Enter)
+echo (if you are prompted to replace a file, type a CAPITAL N, and press Enter)
 echo.
 cd /D "%~dp0"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/microsoft/terminal/releases/download/v1.18.1462.0/Microsoft.WindowsTerminalPreview_1.18.1462.0_x64.zip', 'package.zip')"
@@ -13,7 +13,9 @@ unzip package.zip
 powershell -command "Expand-Archive -Force '%~dp0package.zip' '%~dp0'"
 cd /D "%~dp0"
 cd terminal-1.18.1462.0
+echo.
 echo Starting...
+echo You can close THIS Terminal window now, the script should be open inside another new one.
 .\wt.exe wt.exe "C:\Program Files\Git\git-bash.exe" -c "bash ../multicommittr.sh"
 cd /D "%~dp0"
 
